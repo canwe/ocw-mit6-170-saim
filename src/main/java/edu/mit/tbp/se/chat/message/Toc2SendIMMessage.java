c12a058ce2cd3815f44824c3efe052f870944994
fasehtp@gmail.com <fasehtp@gmail.com@8f958aa4-a4dc-f5d3-de25-255cb592dad5>
2011-02-18 10:49:25 +0000

    Toc2SendIMMessage TocInitDoneMessage

--- src/main/java/edu/mit/tbp/se/chat/message/Toc2SendIMMessage.java
+++ src/main/java/edu/mit/tbp/se/chat/message/Toc2SendIMMessage.java
@@ -1,15 +1,28 @@
 package edu.mit.tbp.se.chat.message;
 
+import edu.mit.tbp.se.chat.Utils;
+
 public class Toc2SendIMMessage extends TOCMessage {
+
+    private final String recipient;
+    private final String msg;
+    private final String auto;
 
     public Toc2SendIMMessage(String destinationUser,
                              String message) {
+        this(destinationUser, message, false);
+    }
 
+    public Toc2SendIMMessage(String destinationUser,
+                             String message,
+                             boolean autoMessage) {
+        this.recipient = Utils.normalise(destinationUser);
+        this.msg = Utils.encodeText(message);
+        auto = autoMessage ? " auto" : "";
     }
 
     @Override
     public String toWireFormat() {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet!");
+        return ("toc_send_im " + recipient + " " + msg + auto);
     }
 }
--- src/main/java/edu/mit/tbp/se/chat/message/TocInitDoneMessage.java
+++ src/main/java/edu/mit/tbp/se/chat/message/TocInitDoneMessage.java
@@ -2,17 +2,17 @@ package edu.mit.tbp.se.chat.message;
 
 public class TocInitDoneMessage extends TOCMessage {
 
+    private static final String CMD = "toc_init_done";
+
     /**
      * Create a new TocInitDoneMessage.
      */
     public TocInitDoneMessage() {
         // pass
-        //TODO
     }
 
     @Override
     public String toWireFormat() {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet!");
+        return(CMD);
     }
 }
